2f8d09157f545a5fb3dc43025c32879ec6677b20
theocita <theocitaallen@gmail.com>
2022-10-26 18:28:25 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-}
+  for(let i = 0; i < array.length; i++){
+    const num = target - array[i];
+    for(let j = i + 1; j < array.length; j++){
+      if (array[j] === num){
+        return true
+      }
+    }
+  }
+  return false 
+ }
 
 /* 
   Write the Big O time complexity of your function here
+  The Big O complexity of this function is quadratic time
 */
 
 /* 
   Add your pseudocode here
+  take one item from the array
+    add them together
+    if they add up to the target number then return it
+    if nott try again until it does
+  
 */
 
 /*
   Add written explanation of your solution here
+  the function should take on element and add it to another element
+
 */
 
 // You can run `node index.js` to view these console logs
